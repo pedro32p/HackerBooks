795832323903d7f3d3455fd8fbc40a0dbf8b828b
Pedro Herranz <peterhr.23@gmail.com>
2017-01-29 17:05:01 +0100

    Primer commit

---
+++ HackerBooks/HackerBooks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackerBooks
+//
+//  Created by Pedro Herranz Roldán on 29/1/17.
+//  Copyright © 2017 Pedro Herranz Roldán. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
